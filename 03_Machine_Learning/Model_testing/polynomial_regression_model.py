2a9542b5a98741225049d8e927274a6b5ced7307
Shevon Cooray <shevonhc@gmail.com>
2026-03-01 07:51:39 +0530

    Update polynomial_regression_model.py

--- 03_Machine_Learning/Model_testing/polynomial_regression_model.py
+++ 03_Machine_Learning/Model_testing/polynomial_regression_model.py
@@ -12,7 +12,7 @@ from sklearn.pipeline import make_pipeline
 # ==========================
 
 # Load dataset
-df = pd.read_csv("chl_2000_3694.csv")
+df = pd.read_csv("../Dataset/chl_2000_3694.csv")
 
 # Extract Chl-a spectrum data
 df_chla = df[df["Chl-a Conc (mg/L)"] > 0]  
