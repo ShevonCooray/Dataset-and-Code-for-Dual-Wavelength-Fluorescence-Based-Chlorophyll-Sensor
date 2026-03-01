fed1b0f022b79d2546ef3d6bedf02b6c519ae978
Shevon Cooray <shevonhc@gmail.com>
2026-03-01 07:52:04 +0530

    Update polynomial_LOO_CV.py

--- 03_Machine_Learning/Model_testing/polynomial_LOO_CV.py
+++ 03_Machine_Learning/Model_testing/polynomial_LOO_CV.py
@@ -12,7 +12,7 @@ from sklearn.pipeline import make_pipeline
 # 1️⃣ Load and Preprocess Data
 # ==========================
 
-df = pd.read_csv("chl_2000_3694.csv")
+df = pd.read_csv("../Dataset/chl_2000_3694.csv")
 
 # Chl-a
 df_chla = df[df["Chl-a Conc (mg/L)"] > 0]
